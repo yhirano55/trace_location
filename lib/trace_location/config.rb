747c3c9ad9bbf8cae7b93caccd4a3a0a3cfb4cb5
Yoshiyuki Hirano <yhirano@me.com>
2019-05-06 02:37:05 +0900

    Update TraceLocation::Config

--- lib/trace_location/config.rb
+++ lib/trace_location/config.rb
@@ -2,11 +2,12 @@
 
 module TraceLocation
   class Config # :nodoc:
-    attr_accessor :root_dir, :dest_dir
+    attr_accessor :root_dir, :dest_dir, :default_format
 
     def initialize
       @root_dir = Dir.pwd
       @dest_dir = Dir.pwd
+      @default_format = :log
     end
   end
 end
